edc5a8a8686533226c4c8344d00a99499949ad38
Rui Pinto <ruipintopsi@outlook.pt>
2019-06-06 15:20:33 +0100

    Added pages , home page development

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
-function runCode()
-{
-    alert("hello world!");
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () =>
+    {
+        //toggle nav
+        nav.classList.toggle('nav-active');
+
+        //animation for the links
+        navLinks.forEach((link, index) =>
+        {
+            if (link.style.animation)
+            {
+                link.style.animation = '';
+            }
+            else 
+            {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+        //burger animation
+        burger.classList.toggle('toggle');
+
+    });
+
+}
+
+navSlide();
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++)
+    {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length)
+    {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
